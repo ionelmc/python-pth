c6c8fd1dd33691ea2ae6998f9b0ede67cdf97ad7
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-09-06 19:49:00 +0300

    Start working on comparison table.

--- src/pth.py
+++ src/pth.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import errno
+import posixpath
 import os
 import shutil
 import sys
@@ -8,6 +9,7 @@ import tempfile
 import time
 import zipfile
 from functools import wraps
+from os import path as ospath
 
 PY2 = sys.version_info[0] == 2
 
@@ -50,7 +52,7 @@ class PathDoesNotExist(PathError):
 
 
 def pth(*parts):
-    path = os.path.join(*parts) if parts else os.path.curdir
+    path = ospath.join(*parts) if parts else ospath.curdir
     if zipfile.is_zipfile(path):
         return ZipPath(Path(path), zipfile.ZipFile(path))
     else:
@@ -84,16 +86,16 @@ def pair_attribute(func, first_func=identity, second_func=identity):
         return first_func(first_value), second_func(second_value)
     return property(pair_attribute_wrapper)
 
-string = os.path.supports_unicode_filenames and unicode or str  # flake8: noqa
+string = str  # flake8: noqa
 
 
 class AbstractPath(string):
-    name = basename = attribute(os.path.basename)
-    dir = dirname = attribute(os.path.dirname)
-    isabs = raw_attribute(os.path.isabs)
-    splitpath = pair_attribute(os.path.split, pth, pth)
-    drive = nth_attribute(os.path.splitdrive, 0)
-    ext = nth_attribute(os.path.splitext, 1)
+    name = basename = attribute(ospath.basename)
+    dir = dirname = attribute(ospath.dirname)
+    isabs = raw_attribute(ospath.isabs)
+    splitpath = pair_attribute(ospath.split, pth, pth)
+    drive = nth_attribute(ospath.splitdrive, 0)
+    ext = nth_attribute(ospath.splitext, 1)
 
     def __repr__(self):
         return 'pth.Path(%r)' % string(self)
@@ -112,31 +114,32 @@ class AbstractPath(string):
 
     @property
     def parts(self):
-        return [pth(part or os.path.sep) for part in self.split(os.path.sep)]
+        return [pth(part or ospath.sep) for part in self.split(ospath.sep)]
 
 
 class Path(AbstractPath):
-    abs = abspath = attribute(os.path.abspath)
-    exists = raw_attribute(os.path.exists)
-    lexists = raw_attribute(os.path.lexists)
-    expanduser = attribute(os.path.expanduser)
-    expandvars = attribute(os.path.expandvars)
-    atime = raw_attribute(os.path.getatime)
-    ctime = raw_attribute(os.path.getctime)
-    mtime = raw_attribute(os.path.getmtime)
-    size = raw_attribute(os.path.getsize)
-    isdir = raw_attribute(os.path.isdir)
-    isfile = raw_attribute(os.path.isfile)
-    islink = raw_attribute(os.path.islink)
-    ismount = raw_attribute(os.path.ismount)
-    joinpath = __div__ = __floordiv__ = __truediv__ = method(os.path.join)
-    normcase = attribute(os.path.normcase)
-    normpath = attribute(os.path.normpath)
-    norm = attribute(lambda self: os.path.normcase(os.path.normpath(self)))
-    real = realpath = attribute(os.path.realpath)
-    rel = relpath = method(os.path.relpath)
-    splitdrive = pair_attribute(os.path.splitdrive, identity, pth)
-    splitext = pair_attribute(os.path.splitext, pth, identity)
+    abs = abspath = attribute(ospath.abspath)
+    exists = raw_attribute(ospath.exists)
+    lexists = raw_attribute(ospath.lexists)
+    expanduser = attribute(ospath.expanduser)
+    expandvars = attribute(ospath.expandvars)
+    atime = raw_attribute(ospath.getatime)
+    ctime = raw_attribute(ospath.getctime)
+    mtime = raw_attribute(ospath.getmtime)
+    size = raw_attribute(ospath.getsize)
+    isdir = raw_attribute(ospath.isdir)
+    isfile = raw_attribute(ospath.isfile)
+    islink = raw_attribute(ospath.islink)
+    ismount = raw_attribute(ospath.ismount)
+    joinpath = __div__ = __floordiv__ = __truediv__ = method(ospath.join)
+    normcase = attribute(ospath.normcase)
+    normpath = attribute(ospath.normpath)
+    norm = attribute(lambda self: ospath.normcase(ospath.normpath(self)))
+    real = realpath = attribute(ospath.realpath)
+    rel = relpath = method(ospath.relpath)
+    same = samefile = method(ospath.samefile)
+    splitdrive = pair_attribute(ospath.splitdrive, identity, pth)
+    splitext = pair_attribute(ospath.splitext, pth, identity)
 
     @property
     def tree(self):
@@ -150,7 +153,7 @@ class Path(AbstractPath):
     @expect_directory
     def list(self):
         for name in os.listdir(self):
-            yield pth(os.path.join(self, name))
+            yield pth(ospath.join(self, name))
 
     def __call__(self, *open_args, **open_kwargs):
         if not self.isdir:
@@ -202,34 +205,34 @@ zippath_attribute = lambda func: property(lambda self: ZipPath(
 
 
 class ZipPath(Path):
-    abs = abspath = zippath_attribute(os.path.abspath)
+    abs = abspath = zippath_attribute(ospath.abspath)
 
     @property
     def exists(self):
-        if not os.path.exists(self.__zippath):
+        if not ospath.exists(self.__zippath):
             return False
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return True
         try:
-            self.__zipobj.getinfo(path)
+            self.__zipinfo(posixpath.normpath(path))
         except KeyError:
             try:
-                self.__zipobj.getinfo(path)
+                self.__zipinfo(posixpath.normpath(path))
             except KeyError:
                 return False
         return True
 
-    expanduser = zippath_attribute(os.path.expanduser)
+    expanduser = zippath_attribute(ospath.expanduser)
     expandvars = property(lambda self: ZipPath(
-        os.path.expandvars(self.__zippath),
+        ospath.expandvars(self.__zippath),
         self.__zipobj,
-        os.path.expandvars(self.__relpath),
+        ospath.expandvars(self.__relpath),
     ))
 
     @property
     def atime(self):
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return self.__zippath.atime
         else:
@@ -237,11 +240,11 @@ class ZipPath(Path):
 
     @property
     def ctime(self):
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return self.__zippath.ctime
         try:
-            zi = self.__zipobj.getinfo(path)
+            zi = self.__zipinfo(posixpath.normpath(path))
         except KeyError as exc:
             raise_(PathDoesNotExist, exc)
         else:
@@ -249,7 +252,7 @@ class ZipPath(Path):
 
     @property
     def mtime(self):
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return self.__zippath.mtime
         else:
@@ -257,11 +260,11 @@ class ZipPath(Path):
 
     @property
     def size(self):
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return self.__zippath.size
         try:
-            zi = self.__zipobj.getinfo(path)
+            zi = self.__zipinfo(posixpath.normpath(path))
         except KeyError as exc:
             raise_(PathDoesNotExist, exc)
         else:
@@ -269,11 +272,11 @@ class ZipPath(Path):
 
     @property
     def isdir(self):
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return True
         try:
-            self.__zipobj.getinfo(path + '/')
+            self.__zipinfo(posixpath.normpath(path) + '/')
         except KeyError:
             return False
         else:
@@ -281,59 +284,57 @@ class ZipPath(Path):
 
     @property
     def isfile(self):
-        path = self.__relpath.rstrip('/')
+        path = self.__relpath.rstrip(ospath.sep)
         if not path:
             return False
         try:
-            self.__zipobj.getinfo(path)
+            self.__zipinfo(posixpath.normpath(path))
         except KeyError:
             return False
         else:
             return True
 
-    islink = raw_attribute(os.path.islink)
-    ismount = raw_attribute(os.path.ismount)
+    islink = raw_attribute(ospath.islink)
+    ismount = raw_attribute(ospath.ismount)
 
     def joinpath(self, *paths):
         for i in reversed(paths):
             if isinstance(i, Path) and i.isabs:
                 return i
-        return ZipPath(self.__zippath, self.__zipobj, os.path.join(self.__relpath, *paths))
+        return ZipPath(self.__zippath, self.__zipobj, ospath.join(self.__relpath, *paths))
 
     __div__ = __floordiv__ = __truediv__ = joinpath
     lexists = unavailable
-    normcase = zippath_attribute(os.path.normcase)
-    normpath = zippath_attribute(os.path.normpath)
-    norm = zippath_attribute(lambda self: os.path.normcase(os.path.normpath(self)))
-    real = realpath = zippath_attribute(os.path.realpath)
+    normcase = zippath_attribute(ospath.normcase)
+    normpath = zippath_attribute(ospath.normpath)
+    norm = zippath_attribute(lambda self: ospath.normcase(ospath.normpath(self)))
+    real = realpath = zippath_attribute(ospath.realpath)
 
     def relpath(self, other):
         if isinstance(other, ZipPath) and other.__zippath == self.__zippath:
-            try:
-                return ZipPath(
-                    "",
-                    self.__zipobj,
-                    os.path.relpath(
-                        os.path.join(self.__zippath, self.__relpath),
-                        os.path.join(self.__zippath, other.__relpath),
-                    )
+            return ZipPath(
+                "",
+                self.__zipobj,
+                ospath.relpath(
+                    ospath.join(self.__zippath, self.__relpath),
+                    ospath.join(self.__zippath, other.__relpath),
                 )
-            except:
-                print(self.__relpath, other.__relpath)
+            )
         else:
-            return pth(os.path.relpath(self, other))
+            return pth(ospath.relpath(self, other))
     rel = relpath
 
-    splitdrive = pair_attribute(os.path.splitdrive, identity, lambda path: ZipPath.from_string(path))
-    splitext = pair_attribute(os.path.splitext, lambda path: ZipPath.from_string(path), identity)
+    splitdrive = pair_attribute(ospath.splitdrive, identity, lambda path: ZipPath.from_string(path))
+    splitext = pair_attribute(ospath.splitext, lambda path: ZipPath.from_string(path), identity)
 
     def __new__(cls, path, zipobj=None, relpath=""):
         if not zipfile.is_zipfile(path):
-            return pth(os.path.join(path, relpath).rstrip('/'))
-        obj = string.__new__(cls, os.path.join(path, relpath).rstrip('/'))
+            return pth(ospath.join(path, relpath).rstrip(ospath.sep))
+        obj = string.__new__(cls, ospath.join(path, relpath).rstrip(ospath.sep))
         obj.__zippath = path
         obj.__zipobj = zipfile.ZipFile(path) if zipobj is None else zipobj
         obj.__relpath = relpath
+        obj.__zipinfo = obj.__zipobj.getinfo
         return obj
 
     @classmethod
@@ -348,7 +349,7 @@ class ZipPath(Path):
         tails.reverse()
         if tails:
             if isinstance(lead, ZipPath):
-                return ZipPath(lead.__zippath, lead.__zipobj, os.path.join(*tails))
+                return ZipPath(lead.__zippath, lead.__zipobj, ospath.join(*tails))
             else:
                 return path
         else:
